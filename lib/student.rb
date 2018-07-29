5b3a92016e11b518fb8fc45c65245d16281374e0
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-07-29 15:23:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,8 +68,8 @@ class Student
 
   def self.count_all_students_in_grade_9
     sql = <<-SQL
-    SELECT COUNT(students.grade) FROM students
-    WHERE 
+    SELECT COUNT(grade) FROM students
+    WHERE grade = 9
     SQL
     DB[:conn].execute(sql)
   end
