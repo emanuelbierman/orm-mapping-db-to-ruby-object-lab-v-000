6f4957ee9ac39c91d3f5f12a65ae5ae902ab2841
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-07-29 15:23:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,6 +75,10 @@ class Student
   end
 
   def self.students_below_12th_grade
-    
+    sql = <<-SQL
+    SELECT COUNT(grade) FROM students
+    WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql)
   end
 end
